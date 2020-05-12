f63829fbd0a9bddbfcbd29c14931e0e87bcdeaee
Jonathan Scott <jonathan.scott@gmail.com>
2020-05-11 23:09:54 -0700

    subheadlines scraping properly

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ const puppeteer = require('puppeteer');
     await page.goto(URL, {waitUntil: 'networkidle2'});
     let data = await page.evaluate(() => {
         let headline = document.querySelector(".screaming-banner-text").innerText;
-        let subHeadlines = document.querySelectorAll(".screaming-banner-text");
+        // let subHeadlines = document.querySelectorAll(".cd__headline");
+        let subHeadlines1 = document.querySelector(".zn__column--idx-1").innerText;
+        let subHeadlines2 = document.querySelector(".zn__column--idx-2").innerText;
 
         return {
             headline,
-            subHeadlines
+            subHeadlines1: subHeadlines1,
+            subHeadlines2: subHeadlines2
         }
     })
     console.log(data);
 
     debugger;
 
-    await browser.close;
+    await browser.close();
 })()
